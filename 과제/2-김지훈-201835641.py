f54cc2b434a8c88f7e0b141014e1035feea26cb0
Jihun Kim <zh1229@naver.com>
2024-05-11 23:51:41 +0900

    feat : 교보문고 온라인 베스트셀러 크롤링 구현

--- 과제/2-김지훈-201835641.py
+++ 과제/2-김지훈-201835641.py
@@ -68,7 +68,7 @@ def main():
     # DataFrame으로 전환 후 columns값 지정
     kyobo_tbl = pd.DataFrame(result, columns=('제목', '저자', '출판사', '출판일자'))
     # csv 파일로 저장
-    kyobo_tbl.to_csv('/Users/hoon/Desktop/빅데이터분석개론실습/실습/docs/kyobo/김지훈-201835641.csv', encoding='cp949', mode='w',
+    kyobo_tbl.to_csv('/Users/hoon/Desktop/빅데이터분석개론실습/실습/docs/kyobo/2-김지훈-201835641.csv', encoding='cp949', mode='w',
                      index=True)
 
     print("\n------------------ 베스트셀러 정보 수집이 완료되었습니다. ------------------")
